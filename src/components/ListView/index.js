d99f0e02b161e46a500f42acf6f3074b913bb192
Linesh <linesh.simpcity@gmail.com>
2017-08-20 17:19:10 +0800

    [Linesh] ✨Feature: implement selectedLocation

--- src/components/ListView/index.js
+++ src/components/ListView/index.js
@@ -4,9 +4,12 @@ export default class ListView {
   constructor(locations) {
     this.locations = locations;
     this.filteredLocations = ko.observableArray(this.locations);
+    this.selectedLocation = ko.observable({});
+
+    this.locationSelected = this.locationSelected.bind(this);
   }
 
-  locationSelected() {
-    console.log('-------- location --------');
+  locationSelected(location) {
+    this.selectedLocation(location);
   }
 }
