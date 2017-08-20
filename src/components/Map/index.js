1b35345600974a0e7d589992d68427d9d62b6d89
Linesh <linesh.simpcity@gmail.com>
2017-08-20 16:59:14 +0800

    [Linesh] ♻️Refactor: move initialLocations to Map component

---
+++ src/components/Map/index.js
@@ -0,0 +1,7 @@
+export default [
+  { location: 'Chengdu',  lat: 30.572, lng: 104.066 },
+  { location: 'Beijing',  lat: 39.904, lng: 116.407 },
+  { location: 'Xi\'an',   lat: 34.341, lng: 108.939 },
+  { location: 'Shenzhen', lat: 22.543, lng: 114.057 },
+  { location: 'Wuhan',    lat: 30.592, lng: 114.305 },
+];
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@
 import ko from 'knockout';
 
 import ListView from './components/ListView';
+import locations from './components/Map';
 import initGoogleMap from './utils/initializer';
 import './index.styl';
-
-const locations = [
-  { location: 'Chengdu',  lat: 30.572, lng: 104.066 },
-  { location: 'Beijing',  lat: 39.904, lng: 116.407 },
-  { location: 'Xi\'an',   lat: 34.341, lng: 108.939 },
-  { location: 'Shenzhen', lat: 22.543, lng: 114.057 },
-  { location: 'Wuhan',    lat: 30.592, lng: 114.305 },
-];
 
 initGoogleMap();
 
