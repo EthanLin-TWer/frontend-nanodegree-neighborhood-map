0c914497c3079ee6a6f0ee23f821015f52260579
Linesh <linesh.simpcity@gmail.com>
2017-08-20 16:57:15 +0800

    [Linesh] ♻️Refactor: move all google map initialization code to initializer.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 /* global locations */
 import ko from 'knockout';
 
-import ListView from './ListView';
+import ListView from './components/ListView';
+import initGoogleMap from './utils/initializer';
 import './index.styl';
-
-const key = 'AIzaSyBJa7HRNHLV9Ir8MS6afvn9yVfIzpBp2po';
 
 const locations = [
   { location: 'Chengdu',  lat: 30.572, lng: 104.066 },
@@ -14,44 +13,6 @@ const locations = [
   { location: 'Wuhan',    lat: 30.592, lng: 114.305 },
 ];
 
-const loadScript = () => {
-  const script = document.createElement('script');
-
-  script.type = 'text/javascript';
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initGoogleMap`;
-
-  document.body.appendChild(script);
-};
-
-const registerCallback = (window) => {
-  window.initGoogleMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: locations[2],
-      zoom: 5
-    });
-
-    locations.forEach(location => {
-      const marker = initMarker(location, map);
-      const infoWindow = initInfoWindow(location.location);
-
-      marker.addListener('click', () => {
-        infoWindow.open(map, marker);
-      });
-    });
-  };
-};
-
-window.addEventListener('load', () => {
-  registerCallback(window);
-  loadScript();
-});
-
-function initMarker(position, map) {
-  return new google.maps.Marker({ position, map });
-}
-
-function initInfoWindow(content) {
-  return new google.maps.InfoWindow({ content });
-}
+initGoogleMap();
 
 ko.applyBindings(new ListView(locations), document.getElementById('list-view'));
---
+++ src/utils/initializer.js
@@ -0,0 +1,49 @@
+const locations = [
+  { location: 'Chengdu',  lat: 30.572, lng: 104.066 },
+  { location: 'Beijing',  lat: 39.904, lng: 116.407 },
+  { location: 'Xi\'an',   lat: 34.341, lng: 108.939 },
+  { location: 'Shenzhen', lat: 22.543, lng: 114.057 },
+  { location: 'Wuhan',    lat: 30.592, lng: 114.305 },
+];
+
+export default function initGoogleMap() {
+  window.addEventListener('load', () => {
+    const callback = registerCallback();
+    loadGoogleMapsAPI(callback);
+  });
+}
+
+const key = 'AIzaSyBJa7HRNHLV9Ir8MS6afvn9yVfIzpBp2po';
+const loadGoogleMapsAPI = (callback) => {
+  const script = document.createElement('script');
+
+  script.type = 'text/javascript';
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=${callback}`;
+
+  document.body.appendChild(script);
+};
+
+const registerCallback = () => {
+  const callbackFunctionName = 'initGoogleMap';
+
+  window[callbackFunctionName] = () => {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: locations[2],
+      zoom: 5
+    });
+
+    locations.forEach(location => {
+      const marker = initMarker(location, map);
+      const infoWindow = initInfoWindow(location.location);
+
+      marker.addListener('click', () => {
+        infoWindow.open(map, marker);
+      });
+    });
+  };
+
+  return callbackFunctionName;
+};
+
+const initMarker = (position, map) => new google.maps.Marker({ position, map });
+const initInfoWindow = (content) => new google.maps.InfoWindow({ content });
