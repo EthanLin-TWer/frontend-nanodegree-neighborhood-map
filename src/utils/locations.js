d58f8d9b1ad6209957222708a0031dd010bf62e4
Linesh <linesh.simpcity@gmail.com>
2017-08-27 19:02:28 +0800

    [Linesh] ✨Feature: update locations

--- src/utils/locations.js
+++ src/utils/locations.js
@@ -1,9 +1,23 @@
 const defaultLocations = [
-  { location: 'Chengdu',  lat: 30.572, lng: 104.066 },
-  { location: 'Beijing',  lat: 39.904, lng: 116.407 },
-  { location: 'Xi\'an',   lat: 34.341, lng: 108.939 },
-  { location: 'Shenzhen', lat: 22.543, lng: 114.057 },
-  { location: 'Wuhan',    lat: 30.592, lng: 114.305 },
+  { location: '兰州 Lanzhou', lat: 103.730, lng: 36.030 },
+  { location: '成都 Chengdu', lat: 104.060, lng: 30.670 },
+  { location: '拉萨 Lasa', lat: 102.730, lng: 25.040 },
+  { location: '贵阳 Guiyang', lat: 106.710, lng: 26.570 },
+  { location: '武汉 Wuhan', lat: 114.310, lng: 30.520 },
+  { location: '郑州 Zhenzhou', lat: 113.650, lng: 34.760 },
+  { location: '济南 Jinan', lat: 117.000, lng: 36.650 },
+  { location: '南京 Nanjing', lat: 118.780, lng: 32.040 },
+  { location: '福州 Fuzhou', lat: 119.300, lng: 26.080 },
+  { location: '广州 Guangzhou', lat: 113.230, lng: 23.160 },
+  { location: '长沙 Changsha', lat: 113.000, lng: 28.210 },
+  { location: '沈阳 Shenyang', lat: 123.380, lng: 41.80 },
+  { location: '西安 Xian', lat: 108.950, lng: 34.270 },
+  { location: '台湾 Taiwan', lat: 121.300, lng: 25.030 },
+  { location: '北京 Beijing', lat: 116.460, lng: 39.920 },
+  { location: '上海 Shanghai', lat: 121.480, lng: 31.220 },
+  { location: '重庆 Chongqing', lat: 106.540, lng: 29.590 },
+  { location: '西藏 Xizang', lat: 91.110, lng: 29.970 },
+  { location: '香港 Hongkong', lat: 114.170, lng: 22.280 },
 ];
 
 export default defaultLocations;
