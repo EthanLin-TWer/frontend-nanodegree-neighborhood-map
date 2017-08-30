6bc77ae3640dcb062f47405e99c167a1709899a0
Linesh <linesh.simpcity@gmail.com>
2017-08-30 12:09:25 +0800

    [Linesh] ♻️Refactor: extract success callback to separate function

--- src/utils/initializer.js
+++ src/utils/initializer.js
@@ -12,21 +12,23 @@ const googleMapFailedLoadingHandler = () => {
   errorMessage.innerHTML = 'Google Map failed to load. Please refresh to try again.';
 };
 
+const initKnockoutMapClass = (google) => {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: defaultLocations[0],
+    zoom: 2
+  });
+
+  const unsplashService = new UnsplashService();
+  // TODO: [Linesh][8/20/17] ugly but the map & markers need to be globally accessible somehow
+  window.mapClass = new Map(map, defaultLocations, unsplashService);
+  window.mapClass.updateVisibleMarkers(defaultLocations);
+};
+
 const key = 'AIzaSyBJa7HRNHLV9Ir8MS6afvn9yVfIzpBp2po';
 export default function loadGoogleMapAPI() {
   GoogleMapsAPILoader({
     libraries: ['places'],
     apiKey: key
-  }).then(google => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: defaultLocations[0],
-      zoom: 2
-    });
-
-    const unsplashService = new UnsplashService();
-    // TODO: [Linesh][8/20/17] ugly but the map & markers need to be globally accessible somehow
-    window.mapClass = new Map(map, defaultLocations, unsplashService);
-    window.mapClass.updateVisibleMarkers(defaultLocations);
-
-  }).catch(googleMapFailedLoadingHandler);
+  }).then(initKnockoutMapClass)
+    .catch(googleMapFailedLoadingHandler);
 }
